10afa7bfbec3b31133c12a4b459f0ce5b0ef028f
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-20 14:47:43 -0500

    Change returned object of getUserInfo

--- frontend+docker/src/api/getUserInfo.jsx
+++ frontend+docker/src/api/getUserInfo.jsx
@@ -4,7 +4,7 @@ import url from './url'
 export default async function getUserInfo(username) {
     try {
         const res = await axios.get(`${url}/get-user-info/${username}`);
-        return res.data;
+        return res.data[1][0];
     } catch (err) {
         console.log(err);
     }
