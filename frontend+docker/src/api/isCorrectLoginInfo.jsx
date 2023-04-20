836c2e3770b493f4dfc71aeb6742155f1d0cc220
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-20 14:50:39 -0500

    Make change to isCorrectLoginInfo

--- frontend+docker/src/api/isCorrectLoginInfo.jsx
+++ frontend+docker/src/api/isCorrectLoginInfo.jsx
@@ -3,7 +3,7 @@ import getUserInfo from "./getUserInfo";
 export default async function isCorrectLoginInfo(username, password) {
     try {
         const user = await getUserInfo(username);
-        const expectedPassword = user.data[1][0].password;
+        const expectedPassword = user.password;
         if (expectedPassword === password) {
             return true;
         } else {
