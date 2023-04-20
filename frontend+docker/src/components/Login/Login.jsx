ceace57643ded66dbb48d352e8fb66b53c99d4e1
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-20 14:55:24 -0500

    Create Login.jsx

---
+++ frontend+docker/src/components/Login/Login.jsx
@@ -0,0 +1,32 @@
+import isCorrectLoginInfo from "../../api/isCorrectLoginInfo";
+
+export default function Login(props) {
+    const { setUser } = props;
+
+    async function attemptLogin(event) {
+        event.preventDefault();
+
+        const username = event.target[0].value;
+        const password = event.target[1].value;
+
+        try {
+            const attempt = await isCorrectLoginInfo(username, password);
+            if (attempt) { setUser(username); }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={(event) => attemptLogin(event)}>
+                <input type="email" placeholder="Email Address"></input>
+                <br></br>
+                <input type="password" placeholder="Password"></input>
+                <br></br>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
