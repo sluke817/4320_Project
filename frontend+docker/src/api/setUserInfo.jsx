af6a85c554fe65b488be0feb2078dc8280834c9e
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-23 14:58:07 -0500

    Create setUserInfo API call

---
+++ frontend+docker/src/api/setUserInfo.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import url from "./url";
+
+/* Example parameter:
+record = {
+    'username': 'kraken',
+    'password': 'password',
+    'fname': 'Kraken',
+    'lname': 'Krakenson',
+    'email': 'somedudenamedkraken@fakeemail.com',
+    'cell_phone': '123-456-7890',
+    'hours_available': '5',
+    'postal_address': '12345 Main Street, Columbia, MO 65201'
+}
+
+Note: Request will create OR edit a user depending on given username
+*/
+export default async function setUserInfo(record) {
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    try {
+        return await axios.post(`${url}/add-edit`, record, config);
+    } catch (err) {
+        console.log(err);
+    }
+}
