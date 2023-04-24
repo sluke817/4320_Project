a786de83242df2b01bb29bd3cd5b9d4b59e1ca0d
Luke Schaefer <luke.schaefer@mail.missouri.edu>
2023-04-23 19:56:54 -0500

    Added details

--- frontend+docker/src/components/ViewAccountDetails.jsx
+++ frontend+docker/src/components/ViewAccountDetails.jsx
@@ -14,69 +14,11 @@ export default function ViewAccountDetails(props) {
                 <fieldset>
                     <legend>Account Details</legend>
                     <div class="form-group">
-                        <label class="col-md-4 control-label">First Name</label>
-                        <div class="col-md-4 inputGroupContainer">
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <p>{details.name}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" >Last Name</label>
-                        <div class="col-md-4 inputGroupContainer">
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <p></p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">E-Mail</label>
-                        <div class="col-md-4 inputGroupContainer">
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                                <p>{user.Lname}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Phone #</label>
-                        <div class="col-md-4 inputGroupContainer">
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
-                                <p>{user.Pn}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Hours Available</label>
-                        <div class="col-md-4 inputGroupContainer">
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span>
-                                <p>{user.Hours}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Address</label>
-                        <div class="col-md-4 inputGroupContainer">
-                            <div class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span>
-                                <p>{user.Addr}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-4 control-label"></label>
-                        <div class="col-md-4">
-                            <button type="submit" class="btn btn-warning" >Edit Details<span class="glyphicon glyphicon-send"></span></button>
-                        </div>
+                        <p><strong>First Name: </strong>{details.fname}</p>
+                        <p><strong>Last Name:</strong>{details.lname}</p>
+                        <p><strong>Email: </strong>{details.email}</p>
+                        <p><strong>Phone Number:</strong>{details.cell_phone}</p>
+                        <p><strong>Address: </strong>{details.postal_address}</p>
                     </div>
 
                 </fieldset>
