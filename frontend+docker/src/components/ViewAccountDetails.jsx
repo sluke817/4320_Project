ea23c07277e068b6ce295812e86db262f2a8cb4a
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-24 13:25:59 -0500

    Add ability to edit profile info

--- frontend+docker/src/components/ViewAccountDetails.jsx
+++ frontend+docker/src/components/ViewAccountDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import getUserInfo from "../api/getUserInfo";
+import setUserInfo from "../api/setUserInfo";
 
 export default function ViewAccountDetails(props) {
     const { user } = props;
@@ -8,20 +9,54 @@ export default function ViewAccountDetails(props) {
         getUserInfo(user).then((response) => setDetails(response));
     }, [user]);
 
+    async function updateDetails(event) {
+        event.preventDefault();
+
+        try {
+            const fName = event.target[1].value;
+            const lName = event.target[2].value;
+            const email = event.target[3].value;
+            const phone = event.target[4].value;
+            const hours = event.target[5].value;
+            const address = event.target[6].value;
+
+            console.log(event.target)
+
+            const record = {
+                'username': user,
+                'password': details.password,
+                'fname': fName,
+                'lname': lName,
+                'email': email,
+                'cell_phone': phone,
+                'hours_available': hours,
+                'postal_address': address
+            }
+            await setUserInfo(record);
+            setDetails(record);
+            alert("Your changes have been saved successfully.")
+        } catch (error) {
+            console.log(error);
+            alert("An error has occurred.")
+        }
+    }
+
     return (
-        <div class="container">
-            <form class="well form-horizontal" action=" " method="get" id="account_details">
+        <div className="container">
+            <form className="well form-horizontal" action=" " method="get" id="account_details" onSubmit={(event) => updateDetails(event)}>
                 <fieldset>
                     <legend>Account Details</legend>
-                    <div class="form-group">
-                        <p><strong>First Name: </strong>{details.fname}</p>
-                        <p><strong>Last Name:</strong>{details.lname}</p>
-                        <p><strong>Email: </strong>{details.email}</p>
-                        <p><strong>Phone Number:</strong>{details.cell_phone}</p>
-                        <p><strong>Address: </strong>{details.postal_address}</p>
+                    <div className="form-group">
+                        <p><strong>First Name: </strong></p><input type="text" defaultValue={details.fname} />
+                        <p><strong>Last Name: </strong></p><input type="text" defaultValue={details.lname} />
+                        <p><strong>Email: </strong></p><input type="text" defaultValue={details.email} />
+                        <p><strong>Phone Number: </strong></p><input type="text" defaultValue={details.cell_phone} />
+                        <p><strong>Available Hours Per Month: </strong></p><input type="text" defaultValue={details.hours_available} />
+                        <p><strong>Address: </strong></p><input type="text" defaultValue={details.postal_address} />
                     </div>
-
                 </fieldset>
+                <br></br>
+                <button>Save Changes</button>
             </form>
         </div>
     )
